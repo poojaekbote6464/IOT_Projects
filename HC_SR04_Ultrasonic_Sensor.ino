df8439892271ba6f616741399ce7fe4975000a81
poojaekbote6464 <poojaekbote6464@gmail.com>
2023-11-05 09:54:11 +0530

    Ultrasonic_Sensor

---
+++ HC_SR04_Ultrasonic_Sensor.ino
@@ -0,0 +1,34 @@
+// Define the pins for the ultrasonic sensor
+const int trigPin = 7; // Trigger pin
+const int echoPin = 10; // Echo pin
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  pinMode(trigPin, OUTPUT); // Set trigger pin as an OUTPUT
+  pinMode(echoPin, INPUT); // Set echo pin as an INPUT
+}
+
+void loop() {
+  // Clear the trigger pin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Send a 10 microsecond pulse to trigger the sensor
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the pulse duration from the echo pin
+  long duration = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance in centimeters
+  // (Speed of sound is approximately 343 meters per second)
+  float distance_cm = duration / 58.2;
+
+  // Print the distance to the serial monitor
+  Serial.print("Distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(500); // Wait for a moment before taking another reading
+}
