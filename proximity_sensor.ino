98dd5cca86f38b57a419d73f66679e193ccb8f96
poojaekbote6464 <poojaekbote6464@gmail.com>
2023-11-05 10:35:08 +0530

    Proximity_Sensor

---
+++ proximity_sensor.ino
@@ -0,0 +1,19 @@
+
+const int irSensorPin = A0; // Analog pin connected to the IR sensor
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(irSensorPin, INPUT);
+}
+
+void loop() {
+  int proximityValue = analogRead(irSensorPin);
+
+  if (proximityValue < 100) { // Adjust the threshold as needed
+    Serial.println("Object detected!");
+  } else {
+    Serial.println("No object detected.");
+  }
+
+  delay(1000); // Delay for a moment before taking another reading
+}
